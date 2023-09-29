4e6e7fc54362a8b936251a47a8dce6eba331d8a6
Tuan Pham <tuan.pham@cerebral.com>
2023-09-29 11:15:44 +0700

    Add tf files

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,13 +1,13 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-course1"
+     bucket = "sre-udacity-terraform-bucket"
      key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
+     region = "us-east-1"
    }
  }
 
  provider "aws" {
-   region = "us-east-2"
+   region = "us-east-1"
 
    default_tags {
      tags = local.tags
